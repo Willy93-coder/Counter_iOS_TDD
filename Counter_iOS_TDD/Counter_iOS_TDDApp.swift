91a3b5de061ee7f867728f17ea7b1acc2cbffdf2
Guillermo Cifre <guillecg13@gmail.com>
2024-11-19 10:56:41 +0100

    Initial Commit

---
+++ Counter_iOS_TDD/Counter_iOS_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_iOS_TDDApp.swift
+//  Counter_iOS_TDD
+//
+//  Created by Guillermo Cifre on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Counter_iOS_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Counter_iOS_TDDUITests/Counter_iOS_TDDUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Counter_iOS_TDDUITestsLaunchTests.swift
+//  Counter_iOS_TDDUITests
+//
+//  Created by Guillermo Cifre on 19/11/24.
+//
+
+import XCTest
+
+final class Counter_iOS_TDDUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
